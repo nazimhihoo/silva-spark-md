3553e3778e3fca7612c8b2c3ee06fdf175e14363
nazimhihoo <muhammadnazimrind@gmail.com>
2025-06-14 23:42:39 +0500

    Update main.js

--- plugins/main.js
+++ plugins/main.js
@@ -234,17 +234,14 @@ async (conn, mek, m, { from, quoted, body, isCmd, command, args, q, isGroup, sen
         await conn.sendMessage(
             from,
             {
-                image: { url: `https://i.imgur.com/PEZ5QL2.jpeg` },
+                image: { url: `https://files.catbox.moe/fhk2br.jpg` },
                 caption: dec,
                 contextInfo: {
                     mentionedJid: [m.sender],
                     forwardingScore: 999,
                     isForwarded: true,
-                    forwardedNewsletterMessageInfo: {
-                        newsletterJid: '120363200367779016@newsletter',
-                        newsletterName: 'SILVA SPARK MD 💖🦄',
-                        serverMessageId: 143
-                    }
+                    forwardedNewsletterMessageInfo: 
+                    
                 }
             },
             { quoted: mek }
